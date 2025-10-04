11f202d5ecbfcd6e7a41413696470e12145883b1
Juan Pedro Lozano <jp@Juans-MacBook-Pro.local>
2025-10-04 20:18:50 +0200

    Fix eslint config and publish to npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,6 @@ module.exports = {
 		ecmaVersion: 2020,
 		sourceType: 'module',
 	},
-	extends: [
-		'plugin:n8n-nodes-base/community',
-	],
-	rules: {
-		'n8n-nodes-base/node-param-display-name-miscased': 'warn',
-		'n8n-nodes-base/node-param-description-miscased': 'warn',
-	},
+	extends: [],
+	rules: {},
 };
